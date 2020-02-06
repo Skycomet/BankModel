1c985c1c7d3849383c5dcdcb34ebb1bfcb29cfd7
knuthmn <knuthmn@MCD210-PC-14.uccc.uc.edu>
2020-02-06 11:59:00 -0500

    I'm lost

--- InClass_2020_02_06/src/cafeBranch/CafeBranch.java
+++ InClass_2020_02_06/src/cafeBranch/CafeBranch.java
@@ -1,5 +1,13 @@
 package cafeBranch;
 
 public class CafeBranch {
+	private String menu;
 
+	public String getMenu() {
+		return menu;
+	}
+
+	public void setMenu(String menu) {
+		this.menu = menu;
+	}
 }
